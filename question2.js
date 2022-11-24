1d1dc5fdd1d30f0da058084b96676275d369da5f
Bhagavath15 <112751473+Bhagavath15@users.noreply.github.com>
2022-11-24 15:41:21 +0530

    Add files via upload

---
+++ question2.js
@@ -0,0 +1,28 @@
+var resume = {
+    "name" : "Bhagavathkumar",
+    "fathersName" :"sathyababu",
+    "age" : "22",
+    "gender" : "male",
+    "city" : "trichy",
+    "qualification":[ {
+        "course" : "B.E Mechanical",
+        "collegeName" : "Government college of Engineering Srirangam",
+        "percentage" :"79.2"
+    },
+    {
+        "course" : "12th",
+        "schoolName" : "Mullai higher secondary school Thottiyam",
+        "percentage" : "87.7"
+    },
+    {
+        "course" : "10th",
+        "schoolName" : "Mullai higher secondary school Thottiyam",
+        "percentage" : "86.7"
+    }
+],
+    "experience" :"no experience",
+    "languagesKnown" : ["tamil","english"],
+    "skills" : ["cad","python"],
+    "hobbies" : ["cricket","travelling"]
+}
+console.log(resume);
